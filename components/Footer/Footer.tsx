6be690eb5c7c834208c39cd42135dfc8d46c7203
Pratham Mishra <prathammishra178@gmail.com>
2022-08-10 16:03:38 +0530

    feat-footer-section

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,9 @@
 import styles from "./footer.module.scss"
 
 export const Footer = ({}) => {
-  return <footer className={styles.footer}></footer>;
+  return (
+  <footer className={styles.footer}>
+    
+  </footer>
+  );
 };
