69e4fbace282b26aca52654589520c0bece70c81
Ravindra Nag <ravindranag52@gmail.com>
2022-08-12 23:11:49 +0530

    feat: footer component

---
+++ components/Footer/LinkContainer.tsx
@@ -0,0 +1,36 @@
+import { Stack, Typography } from "@mui/material";
+import { FC } from "react";
+import { IFooterLink } from "./footer-links";
+import { SubLink } from "./SubLink";
+
+interface Props {
+  footerLink: IFooterLink
+}
+
+export const LinkContainer: FC<Props> = ({footerLink}) => {
+  return (
+    <Stack
+      gap='20px'
+      // flexGrow={1}
+      width='100%'
+      maxWidth={155}
+    >
+      <Typography
+        variant='h6'
+        color='white'
+      >
+        {footerLink.title}
+      </Typography>
+      <Stack
+        gap='10px'
+      >
+        { footerLink.links.map(link => (
+          <SubLink 
+            key={ link.name }
+            link={ link }
+          />
+        )) }
+      </Stack>
+    </Stack>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Stack } from "@mui/material";
 import { Footer } from "components/Footer";
 import type { NextPage } from "next";
 import Head from "next/head";
@@ -5,17 +6,11 @@ import styles from "./index.module.scss";
 
 const Home: NextPage = () => {
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>WalkingPal</title>
-				<meta name="description" content="WalkingPal ka desc" />
-				<meta name="keywords" content="home, walking, pal, buddy, walk" />
-			</Head>
-
-			<main className={styles.main}></main>
-
-			<Footer />
-		</div>
+    <Stack
+      minHeight='100vh'
+    >
+      <Footer />
+    </Stack>
 	);
 };
 
