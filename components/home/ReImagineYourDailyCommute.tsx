261ae6cc3cd4aa6cc03cf91237e7ff9535acb257
Ravindra Nag <ravindranag52@gmail.com>
2022-08-16 08:13:52 +0530

    feat: commute cards done

--- components/home/ReImagineYourDailyCommute.tsx
+++ components/home/ReImagineYourDailyCommute.tsx
@@ -1,5 +1,51 @@
-import { Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import { FC } from "react";
+import Community from "./reimagine/Community";
+import { CommuteCard } from "./reimagine/CommuteCard";
+
+export interface ICommuteCard {
+  id: number
+  text: string
+  highlight: string
+  img: string
+  bgcolor: string
+  textColor: string
+}
+
+const commuteCards: ICommuteCard[] = [
+  {
+    id: 0,
+    text: 'For',
+    highlight: 'shopping',
+    img: '/images/wp_shopping.png',
+    bgcolor: '#8DD8D7',
+    textColor: '#67CBC9'
+  },
+  {
+    id: 1,
+    text: 'For',
+    highlight: 'work',
+    img: '/images/wp_promotion.png',
+    bgcolor: '#FFE3E3',
+    textColor: '#FFACAC'
+  },
+  {
+    id: 2,
+    text: 'For',
+    highlight: 'traveling',
+    img: '/images/wp_bagpack.png',
+    bgcolor: '#F8CF7D',
+    textColor: '#F6BF51'
+  },
+  {
+    id: 3,
+    text: 'Or just a',
+    highlight: 'walk!',
+    img: '/images/wp_footsteps.png',
+    bgcolor: '#FF8282',
+    textColor: '#FF5959'
+  },
+]
 
 export const ReImagineYourDailyCommute: FC = () => {
   return (
@@ -20,6 +66,29 @@ export const ReImagineYourDailyCommute: FC = () => {
           {' '}commute
         </Typography>
       </Typography>
+      <Grid
+        container
+        spacing='16px'
+        justifyContent='space-between'
+      >
+        { commuteCards.map(cc => (
+          <Grid
+            key={cc.id}
+            item
+            xs={6}
+            md={3}
+            // maxWidth={256}
+            // display='flex'
+            // justifyContent='center'
+            // alignItems='center'
+          >
+            <CommuteCard
+              cc={cc}
+            />
+          </Grid>
+        )) }
+      </Grid>
+      <Community />
     </Stack>
   )
 }
--- components/home/reimagine/CommuteCard.tsx
+++ components/home/reimagine/CommuteCard.tsx
@@ -1,16 +1,72 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { FC } from "react";
+import { ICommuteCard } from "../ReImagineYourDailyCommute";
 
-export const CommuteCard: FC = () => {
+
+
+export const CommuteCard: FC<{cc: ICommuteCard}> = ({cc}) => {
+
+
+
   return (
-    <Stack>
-      <Box
+    <Stack
+      padding='14px'
+      bgcolor={cc.bgcolor}
+      maxWidth='256px'
+      width='100%'
+      borderRadius='30px'
+    >
+      <Stack
         sx={{
-          flexGrow: 1
+          height: '112px',
+          backgroundColor: '#FFFFFF'
+        }}
+        justifyContent='center'
+        alignItems='center'
+        borderRadius='20px 20px 0 0' 
+      >
+        <Stack
+          direction='row'
+          gap='4px'
+        >
+          <Typography
+            fontWeight={500}
+          >
+            {cc.text}
+          </Typography>
+          <Typography
+            fontWeight={500}
+            color={cc.textColor}
+          >
+            {cc.highlight}
+          </Typography>
+        </Stack>
+      </Stack>
+      <Stack
+        height='180px'
+        bgcolor={cc.bgcolor}
+        justifyContent='center'
+        alignItems='center'
+        padding={{
+          xs: '24px 0',
+          md: '24px'
         }}
       >
-        
-      </Box>
+        <Box  
+          component='img'
+          src={cc.img}
+          sx={{
+            aspectRatio: 1,
+            width: {
+              xs: '100%',
+              md: 'unset'
+            },
+            height: {
+              md: '100%'
+            }
+          }}
+        />
+      </Stack>
     </Stack>
   )
 }
