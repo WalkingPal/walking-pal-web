5c27c74187a6a238b588e1125663818641ee870f
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-08-16 15:56:03 +0530

    feat: add footer bottom part + remove footer links

--- assets/svg/index.ts
+++ assets/svg/index.ts
@@ -2,5 +2,7 @@ export { default as Logo } from "./logo.svg";
 export { default as Wordmark } from "./wordmark.svg";
 export { default as Hamburger } from "./hamburger.svg";
 export { default as Play } from "./play.svg";
-
-
+export { default as Twitter } from "./twitter.svg";
+export { default as YouTube } from "./youtube.svg";
+export { default as Linkedin } from "./linkedin.svg";
+export { default as Insta } from "./insta.svg";
