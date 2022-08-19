67671fdb5b75ad1a55b9e227e5c0df63d4118d46
Pratham Mishra <prathammishra178@gmail.com>
2022-08-19 21:35:13 +0530

    feat: gallary-section

--- pages/about/components/gallary.tsx
+++ pages/about/components/gallary.tsx
@@ -1,25 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
-/* eslint-disable react/jsx-no-comment-textnodes */
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+import { pic1, pic2, pic3, pic4 } from "assets/png";
 import Image, { ImageProps } from "next/image";
-import { pic1 } from "assets/png";
-import { pic2 } from "assets/png";
-import { pic3 } from "assets/png";
-import { pic4 } from "assets/png";
+import React, { FC } from "react";
+import styles from "./gallary.module.scss";
 
-const Item = styled(Paper)(({ theme }) => ({
-	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-	...theme.typography.body2,
-	padding: theme.spacing(1),
-	textAlign: "center",
-	color: theme.palette.text.secondary,
-}));
+interface IImg extends ImageProps {
+	className?: string;
+	display?: "flex" | "grid";
+}
 
-const Img = (props: any) => {
+const Img: FC<IImg> = props => {
 	return (
 		<span
 			className={props.className}
@@ -30,31 +19,13 @@ const Img = (props: any) => {
 	);
 };
 
-export const Gallary = ({}) => {
+export const Gallary: FC = ({}) => {
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<Grid container spacing={2}>
-				<Grid item xs={4}>
-					<Item>
-						<Img src={pic1} />
-					</Item>
-				</Grid>
-				<Grid item xs={4}>
-					<Item>
-						<Img src={pic2} />
-					</Item>
-				</Grid>
-				<Grid item xs={4}>
-					<Item>
-						<Img src={pic4} />
-					</Item>
-				</Grid>
-				<Grid item xs={8}>
-					<Item>
-						<Img src={pic3} />
-					</Item>
-				</Grid>
-			</Grid>
-		</Box>
+		<div className={styles.intro}>
+			<Img className={styles.mockup} src={pic1} />
+			<Img className={styles.credit} src={pic2} />
+			<Img className={styles.screens2} src={pic3} />
+			<Img className={styles.noSm} src={pic4} />
+		</div>
 	);
 };
