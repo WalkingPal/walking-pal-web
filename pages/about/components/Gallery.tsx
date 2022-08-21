90402297cb7ca02856443d6c17f01919c3cb4cbb
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-08-21 13:45:48 +0530

    style(about): gallery grids re-impl

--- pages/about/components/Gallery.tsx
+++ pages/about/components/Gallery.tsx
@@ -1,19 +1,17 @@
-import { pic1, pic2, pic3, pic4 } from "assets/png";
+import { Typography } from "@mui/material";
+import { about1, about2, about3, about4 } from "assets/png";
 import Image, { ImageProps } from "next/image";
-import React, { FC } from "react";
+import React, { CSSProperties, FC, useRef } from "react";
 import styles from "../about.module.scss";
 
 interface IImg extends ImageProps {
 	className?: string;
-	display?: "flex" | "grid";
+	wrapStyle?: CSSProperties;
 }
 
 const Img: FC<IImg> = props => {
 	return (
-		<span
-			className={props.className}
-			style={{ minWidth: props.width, display: props.display }}
-		>
+		<span className={props.className} style={props.wrapStyle}>
 			<Image alt="" {...props} />
 		</span>
 	);
@@ -23,18 +21,18 @@ export const Gallery: FC = ({}) => {
 	return (
 		<div className={styles.main}>
 			<div className={styles.gallery}>
-				<Img className={styles.pic1} src={pic1} />
-				<Img className={styles.pic2} src={pic2} />
-				<Img className={styles.pic4} src={pic4} />
-				<Img className={styles.pic3} src={pic3} />
-				<span className={styles.typo}>
-					Every groundbreaking App begins with a series of small ideas that are{" "}
-					combined into an all inclusive one. At WalkingPal, we wanted to change
-					how consumers see the concept of Walking to commute daily, by offering
-					a simple and user-friendly platform they can trust. Now having a
-					Walking Buddy anytime and anywhere is easier than ever!
-				</span>
+				<Img className={styles.about1} src={about1} />
+				<Img className={styles.about2} src={about2} />
+				<Img className={styles.about3} src={about3} />
+				<Img className={styles.about4} src={about4} />
 			</div>
+			<Typography variant="h6" minWidth="300px" width="70%" textAlign="center">
+				Every groundbreaking App begins with a series of small ideas that are
+				combined into an all inclusive one. At WalkingPal, we wanted to change
+				how consumers see the concept of Walking to commute daily, by offering a
+				simple and user-friendly platform they can trust. Now having a Walking
+				Buddy anytime and anywhere is easier than ever!
+			</Typography>
 		</div>
 	);
 };
