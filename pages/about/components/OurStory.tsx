2bff53a4368f1d3b7def031627bbca77233ec1c3
Asmi-1 <anusmitajena1912@gmail.com>
2022-08-21 13:42:30 +0530

    feat:  ourstory-section

--- pages/about/components/OurStory.tsx
+++ pages/about/components/OurStory.tsx
@@ -29,51 +29,41 @@ export const OurStory: FC = ({}) => {
 			</Typography>
 
 			<div className={styles.story1}>
-				<div>
-					<Img className={styles.mockup} src={story1} />
-				</div>
-				<div className={styles.typo1}>
-					<Typography variant="h5">
-						Sometimes walking alone on a sidewalk feels very exhausting and
-						lonely, but if there is a buddy by our side the journey however long
-						it may be, it doesn-t bother us anymore. But finding the one to
-						commute daily is like finding a needle in the haystack, We won-t be
-						able to help you to find your needle but we can certainly help you
-						find a walking buddy anytime you need and I am sure you have come
-						across this problem atleast once in your life.{" "}
-					</Typography>
-				</div>
+				<Img className={styles.mockup} src={story1} />
+
+				<Typography variant="h5" maxWidth="620px" textAlign="justify">
+					Sometimes walking alone on a sidewalk feels very exhausting and
+					lonely, but if there is a buddy by our side the journey however long
+					it may be, it doesn-t bother us anymore. But finding the one to
+					commute daily is like finding a needle in the haystack, We won-t be
+					able to help you to find your needle but we can certainly help you
+					find a walking buddy anytime you need and I am sure you have come
+					across this problem atleast once in your life.{" "}
+				</Typography>
 			</div>
 
 			<div className={styles.story2}>
-				<div>
-					<Img className={styles.credit} src={story2} />
-				</div>
-				<div className={styles.typo2}>
-					<Typography variant="h5">
-						You will be glad to hear that you are not alone there are many
-						people out there too who do feel the same. WalkingPal is the
-						antidote to all those problems of our-s, it joins people like us who
-						have a similar problem so that we can complete the journey easily
-						together.{" "}
-					</Typography>
-				</div>
+				<Img className={styles.credit} src={story2} />
+
+				<Typography variant="h5" maxWidth="523px" textAlign="justify">
+					You will be glad to hear that you are not alone there are many people
+					out there too who do feel the same. WalkingPal is the antidote to all
+					those problems of our-s, it joins people like us who have a similar
+					problem so that we can complete the journey easily together.{" "}
+				</Typography>
 			</div>
 
 			<div className={styles.story3}>
-				<div>
-					<Img className={styles.screens2} src={story3} />
-				</div>
-				<div className={styles.typo3}>
-					<Typography variant="h5">
-						By making walking more fun and enjoyable it-s a great way to
-						complete the last mile which is both cheap (free actually) and
-						eco-friendly. Oh, and it-s good for your health too. It-s a great
-						way to connect and meet new people, make new friends, relationships
-						To explore and talk about the local food, places & culture that too
-						completing your journey at the same time while walking together.
-					</Typography>
-				</div>
+				<Img className={styles.screens2} src={story3} />
+
+				<Typography variant="h5" maxWidth="592px" textAlign="justify">
+					By making walking more fun and enjoyable it-s a great way to complete
+					the last mile which is both cheap (free actually) and eco-friendly.
+					Oh, and it-s good for your health too. It-s a great way to connect and
+					meet new people, make new friends, relationships To explore and talk
+					about the local food, places & culture that too completing your
+					journey at the same time while walking together.
+				</Typography>
 			</div>
 		</div>
 	);
