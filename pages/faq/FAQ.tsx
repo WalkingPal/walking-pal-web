f753be97cec722138fdafaec075006246ec27910
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-08-31 01:56:28 +0530

    feat(feedback): add new faqs (content)

--- pages/faq/FAQ.tsx
+++ pages/faq/FAQ.tsx
@@ -10,12 +10,39 @@ const generalFAQs = [
 		desc: "Yes, we don’t charge a single penny to our users.",
 	},
 	{
-		faq: "What Platforms do we support",
-		desc: "WalkingPal App is supported on Android only. We are planning to target iOS users in the near future.",
+		faq: "When will it be publicly available to use?",
+		desc: "We are currently in private beta stage. You could either register as an early user and become a beta tester or wait for the public release soon.",
 	},
 	{
-		faq: "Where can I download the App",
-		desc: "App will be available on Google Play Store post our App's beta-testing phase.",
+		faq: "How walkingpal works?",
+		desc: "WalkingPal scans all the nearly people that are in need of a walking buddy, and it connects you with your nearest walking buddy going towards the same direction and destination as you. \nP.S. We won’t be sharing your exact destination with anyone for obvious security reasons.",
+	},
+	{
+		faq: "What Platforms do we support?",
+		desc: "We will be launching our android app first and subsequently it will be available on the Apple app store in future.",
+	},
+	{
+		faq: "Where can we download the app?",
+		desc: "You can download WalkingPal on the Google play store post our private beta test. As of now you can register for it and we will notify you when it is available.",
+	},
+];
+
+const TrustnSafetyFAQs = [
+	{
+		faq: "Is it safe to use?",
+		desc: "No technology is 100% safe for everyone but we are working hard to make it at-least 99% safe for all our users. Your safety is our first priority.",
+	},
+	{
+		faq: "What about fake users?",
+		desc: "In order to minimise fake users, we will be verifying the identity of every user by a government approved I’d, like passport, Aadhar Card, Etc. So that no one ends up with an undocumented person.",
+	},
+	// {
+	// 	faq: "How will you ensure the safety of the users?",
+	// 	desc: "We have listed below the steps that we are taking to ensure your safety:\n\t• I’D verification of every user.\n\t• We will be tracking your whole journey once you get connected with a walking buddy, and you can also share you live location to your friends and family as well to be extra safe.\n\t• After you start walking with your walking buddy there will be an SOS bubble popup on the screen (kinda like a messenger bubble). If you are in danger or at an uncomfortable position you can just tap on that bubble and a notification will be sent to all your emergence contacts and to us as well and we will try our best to help you out in every possible way.",
+	// },
+	{
+		faq: "What if I don’t want to go alone with a stranger?",
+		desc: "If you are not comfortable walking alone with a stranger, we also have an option to create a walking group. You can add 2-3 more people to join with you in your journey.",
 	},
 ];
 
@@ -36,7 +63,8 @@ export const FAQ: NextPage = () => {
 
 			<main style={{ marginBottom: 80 }}>
 				<Hero />
-				<FAQsection title="General" faqs={generalFAQs} />
+				<FAQsection title="General" faqs={generalFAQs} isOpen />
+				<FAQsection title="Trust and Safety" faqs={TrustnSafetyFAQs} />
 				<FeedbackForm />
 			</main>
 		</>
