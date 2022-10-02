0f0c7e3c94baa0b3e75d9bd333ccac008235fcd3
Emma Dawson <emma.l.dawson@gmail.com>
2022-10-02 20:29:30 +0200

    fix: use correct semantic elements in header nav

--- components/Header/components/List1.tsx
+++ components/Header/components/List1.tsx
@@ -1,6 +1,11 @@
-import { List, ListItemButton, ListItemText, Typography } from "@mui/material";
+import {
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemText,
+	Typography,
+} from "@mui/material";
 import { links } from "components/Header";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { FC, MouseEvent } from "react";
 
@@ -12,7 +17,7 @@ export const List1: FC<IList1> = ({ mobile }) => {
 	const router = useRouter();
 
 	const handleClick = (
-		e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>,
+		e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>,
 		route: string,
 	) => {
 		e.preventDefault();
@@ -36,24 +41,29 @@ export const List1: FC<IList1> = ({ mobile }) => {
 		>
 			{links.map(({ name, route }) => {
 				return (
-					<ListItemButton
-						key={name}
-						onClick={e => handleClick(e, route)}
-						sx={{ borderRadius: 1 }}
-					>
-						<ListItemText>
-							<Typography
-								variant="body2"
-								sx={{
-									color: router.route === route ? "primary.main" : "#828282",
-									textTransform: "capitalize",
-									px: 2,
-								}}
-							>
-								<Link href={route}>{name}</Link>
-							</Typography>
-						</ListItemText>
-					</ListItemButton>
+					<ListItem key={name}>
+						<ListItemButton
+							onClick={(
+								e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>,
+							) => handleClick(e, route)}
+							sx={{ borderRadius: 1 }}
+							component="a"
+							role="link"
+						>
+							<ListItemText>
+								<Typography
+									variant="body2"
+									sx={{
+										color: router.route === route ? "primary.main" : "#828282",
+										textTransform: "capitalize",
+										px: 2,
+									}}
+								>
+									{name}
+								</Typography>
+							</ListItemText>
+						</ListItemButton>
+					</ListItem>
 				);
 			})}
 		</List>
