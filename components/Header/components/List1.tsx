887b91239b5977465723bede69508bae32a5bef6
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-10-06 01:37:58 +0530

    feat: navitem dropdown

--- components/Header/components/List1.tsx
+++ components/Header/components/List1.tsx
@@ -3,24 +3,24 @@ import {
 	ListItem,
 	ListItemButton,
 	ListItemText,
+	Menu,
+	MenuItem,
 	Typography,
 } from "@mui/material";
 import { useRouter } from "next/router";
-import { FC, MouseEvent } from "react";
-import { Dropdown } from "./Dropdown";
+import { FC, MouseEvent, useState } from "react";
 
 export const links = [
-	{ name: "home", route: "/" },
-	{ name: "about", route: "/about" },
-	{ name: "FAQ", route: "/faq" },
+	{ name: "home", pageroute: "/" },
+	{ name: "about", pageroute: "/about" },
 	{
 		name: "company",
-		route: "/company",
+		pageroute: "/company",
 		subroutes: [
-			{ name: "social impact", route: "#social" },
-			{ name: "our team", route: "#team" },
-			{ name: "careers", route: "#career" },
-			{ name: "contact", route: "#contact" },
+			{ name: "social impact", subroute: "#social" },
+			{ name: "our team", subroute: "#team" },
+			{ name: "careers", subroute: "#career" },
+			{ name: "contact", subroute: "#contact" },
 		],
 	},
 ];
@@ -30,63 +30,95 @@ interface IList1 {
 }
 
 export const List1: FC<IList1> = ({ mobile }) => {
-	const router = useRouter();
-
-	const handleClick = (
-		e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>,
-		route: string,
-	) => {
-		e.preventDefault();
-		router.push(route);
-	};
-
 	return (
 		<List
 			sx={
 				mobile
-					? {
-							display: "grid",
-							width: "100%",
-					  }
-					: {
-							display: "flex",
-							justifyContent: "flex-start",
-							width: "unset",
-					  }
+					? { display: "grid", width: "100%" }
+					: { display: "flex", justifyContent: "flex-start", width: "unset" }
 			}
 		>
-			{links.map(({ name, route, subroutes }) => {
+			{links.map(link => {
 				return (
-					<ListItem key={name}>
-						{subroutes ? (
-							<Dropdown />
-						) : (
-							<ListItemButton
-								onClick={(
-									e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>,
-								) => handleClick(e, route)}
-								sx={{ borderRadius: 1 }}
-								component="a"
-								role="link"
-							>
-								<ListItemText>
-									<Typography
-										variant="body2"
-										sx={{
-											color:
-												router.route === route ? "primary.main" : "#828282",
-											textTransform: "capitalize",
-											px: 2,
-										}}
-									>
-										{name}
-									</Typography>
-								</ListItemText>
-							</ListItemButton>
-						)}
+					<ListItem key={link.name}>
+						<L1Item {...link} />
 					</ListItem>
 				);
 			})}
 		</List>
+	);
+};
+
+interface IL1Item {
+	name: string;
+	pageroute: string;
+	subroutes?: {
+		name: string;
+		subroute: string;
+	}[];
+}
+
+export const L1Item: FC<IL1Item> = ({ name, pageroute, subroutes }) => {
+	const router = useRouter();
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+
+	const handleClick = (
+		e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>,
+	) => {
+		e.stopPropagation();
+		e.preventDefault();
+		setAnchorEl(e.currentTarget);
+		router.route !== pageroute && router.push(pageroute);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	return (
+		<>
+			<ListItemButton
+				onClick={handleClick}
+				sx={{ borderRadius: 1 }}
+				component="a"
+				role="link"
+			>
+				<ListItemText>
+					<Typography
+						variant="body2"
+						sx={{
+							color: router.route === pageroute ? "primary.main" : "#828282",
+							textTransform: "capitalize",
+							px: 2,
+						}}
+					>
+						{name}
+					</Typography>
+				</ListItemText>
+			</ListItemButton>
+			{subroutes && (
+				<Menu
+					open={open}
+					onClose={handleClose}
+					anchorEl={anchorEl}
+					sx={{ zIndex: 99999 }}
+				>
+					{subroutes.map(({ name, subroute }) => (
+						<MenuItem
+							key={name}
+							onClick={() => {
+								router.route !== pageroute && router.push(pageroute + subroute);
+								handleClose();
+							}}
+							component="a"
+							role="link"
+							href={subroute}
+							sx={{ textTransform: "capitalize" }}
+						>
+							{name}
+						</MenuItem>
+					))}
+				</Menu>
+			)}
+		</>
 	);
 };
