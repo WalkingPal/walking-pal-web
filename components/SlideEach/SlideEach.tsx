8bc179e48f13218a2756872a9d6d0c45e364328a
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-10-07 18:14:05 +0530

    Update SlideEach.tsx

--- components/SlideEach/SlideEach.tsx
+++ components/SlideEach/SlideEach.tsx
@@ -2,9 +2,7 @@ import { SlideIn } from "components/SlideIn";
 import { ISlideIn } from "components/SlideIn/SlideIn";
 import React, { FC, ReactNode } from "react";
 
-interface ISlideEach extends ISlideIn {
-	children: ReactNode;
-}
+interface ISlideEach extends ISlideIn {}
 
 export const SlideEach: FC<ISlideEach> = ({ children, ...slideProps }) => {
 	return (
