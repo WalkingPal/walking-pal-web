ae3fffe75e9334962a201e21f877524410ec1bb7
Manthan Kuber <manthankuber873@gmail.com>
2022-10-11 19:24:07 +0530

    feat: add faq endpoint data validation

--- pages/api/feedback/index.pg.ts
+++ pages/api/feedback/index.pg.ts
@@ -2,10 +2,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 import db from "utils/db";
 import admin from "firebase-admin";
 import isReCaptchaValid from "pages/api/utils/reCaptcha";
+import { z, ZodIssue } from "zod";
 
 interface ResponseData {
 	message: string;
+	errors?: ZodIssue[];
 }
+
+const formDataSchema = z.object({
+	name: z
+		.string()
+		.min(3, { message: "Name must be greater the 2 characters" })
+		.regex(/^[aA-zZ\s]+$/, { message: "Enter a valid Name" }),
+	email: z.string().min(1).email({ message: "Please enter valid Email" }),
+	phone: z.string().optional(),
+	message: z.string().min(1),
+});
+
+const captchaSchema = z.string().min(1);
+
 const registerFeedback = async (
 	req: NextApiRequest,
 	res: NextApiResponse<ResponseData>,
@@ -14,21 +29,29 @@ const registerFeedback = async (
 		body: { formData, captcha },
 		method,
 	} = req;
+
 	if (method !== "POST")
 		return res.status(404).json({
 			message: "Invalid HTTP Method. Only POST method is Accepted.",
 		});
 
-	// do zod and neccesary validations here & email check via any free to use available apis too
+	const parsedFormData = formDataSchema.safeParse(formData);
+	const parsedCaptcha = captchaSchema.safeParse(captcha);
 
-	// if captcha is missing OR isAnyFieldEmpty check
-	if (!Object.values(formData).every(x => !!x))
+	if (!parsedFormData.success)
 		return res.status(422).json({
-			message: "Unproccesable request, Missing Fields",
+			message: "Unproccesable Request",
+			errors: parsedFormData.error.issues,
+		});
+
+	if (!parsedCaptcha.success)
+		return res.status(422).json({
+			message: "Unproccesable Request",
+			errors: parsedCaptcha.error.issues,
 		});
 
 	if (!(await isReCaptchaValid(captcha)))
-		return res.status(422).json({
+		return res.status(422).send({
 			message: "Unproccesable request, Invalid captcha code",
 		});
 
