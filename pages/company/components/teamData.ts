4d031fcc22b4b401c27efbb8d14ebc259ca10a77
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2022-10-26 03:29:26 +0530

    feat(company): responsive Team Section

---
+++ pages/company/components/teamData.ts
@@ -0,0 +1,91 @@
+import { ashutosh, junaid, members } from "assets/png";
+export const teamData = {
+	vp: [
+		{
+			name: "Junaid Ahmed",
+			desg: "Founter and CEO",
+			src: junaid,
+			bgcolor: "#FF5959",
+		},
+		{
+			name: "Ashutosh Khanduala",
+			desg: "VP of Engineering",
+			src: ashutosh,
+			bgcolor: "#67CBC9",
+		},
+	],
+	members: [
+		{
+			name: "Flevie Patnaik",
+			desg: "Content Producer",
+			src: members["flevie"],
+			bgcolor: "#FF5959",
+		},
+		{
+			name: "Misty Ghose",
+			desg: "Social media growth",
+			src: members["misty"],
+			bgcolor: "#F6BF51",
+		},
+		{
+			name: "Arpit Anshuman",
+			desg: "Non-Tech Contributor",
+			src: members["arpit"],
+			bgcolor: "#67CBC9",
+		},
+		{
+			name: "Astha Niharika",
+			desg: "Web Developer",
+			src: members["astha"],
+			bgcolor: "#FF5959",
+		},
+		{
+			name: "Akhilesh Nair",
+			desg: "Backend Developer",
+			src: members["akhilesh"],
+			bgcolor: "#F6BF51",
+		},
+		{
+			name: "Pratham Mishra",
+			desg: "Frontend Developer",
+			src: members["pratham"],
+			bgcolor: "#F6BF51",
+		},
+		{
+			name: "Pallav Patra",
+			desg: "Frontend Developer",
+			src: members["pallav"],
+			bgcolor: "#FF5959",
+		},
+		{
+			name: "Asmi Jena",
+			desg: "Frontend Developer",
+			src: members["asmi"],
+			bgcolor: "#F6BF51",
+		},
+		{
+			name: "Pratikash Panda",
+			desg: "Web Developer",
+			src: members["pratikash"],
+			bgcolor: "#67CBC9",
+		},
+		{
+			name: "Prachi Suman",
+			desg: "Web Developer",
+			src: members["prachi"],
+			bgcolor: "#FF5959",
+		},
+		{
+			name: "Ravindra Nag",
+			desg: "Frontend Developer",
+			src: members["ravindra"],
+			bgcolor: "#67CBC9",
+		},
+		{
+			name: "Ronak Mallick",
+			desg: "Graphic Designer",
+			src: members["ronak"],
+			bgcolor: "#67CBC9",
+		},
+	],
+};
